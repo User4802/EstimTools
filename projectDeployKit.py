cb80f58c713888ee1a21e991328ed2c05f11ed02
user4802 <24318149+User4802@users.noreply.github.com>
2018-09-10 18:06:51 -0400

    added express deploy from argv

--- projectDeployKit.py
+++ projectDeployKit.py
@@ -1,12 +1,57 @@
 version = "0.3"
+
+from sys import argv
+script, initMethod = argv
 import os
 
-print("Project Deploy Kit V{0}".format(version))
 
-main_path = input("Main Path ? ")
 
-os.mkdir(main_path)
-os.mkdir(main_path + "/plans")
-os.mkdir(main_path + "/documents")
-os.mkdir(main_path + "/soumission")
-os.mkdir(main_path + "/cotations")
+im = str(initMethod)
+
+def methodBasic():
+
+    print("Project Deploy Kit V{0}".format(version))
+
+    express_path = input("Main Path ? ")
+
+    os.mkdir(main_path)
+    os.mkdir(main_path + "/plans")
+    os.mkdir(main_path + "/documents")
+    os.mkdir(main_path + "/soumission")
+    os.mkdir(main_path + "/cotations")
+
+def methodCustom():
+
+    print("Project Deploy Kit V{0}".format(version))
+
+    main_path = input("Main Path ? ")
+
+    os.mkdir(main_path)
+    os.mkdir(main_path + "/plans")
+    os.mkdir(main_path + "/documents")
+    os.mkdir(main_path + "/soumission")
+    os.mkdir(main_path + "/cotations")
+
+def methodExpress():
+
+    express_path = os.getcwd() + "/" + im
+    os.mkdir(express_path)
+    os.mkdir(express_path + "/plans")
+    os.mkdir(express_path + "/documents")
+    os.mkdir(express_path + "/soumission")
+    os.mkdir(express_path + "/cotations")
+
+def methodChoice(im):
+    if im == "basic":
+        #run the basic setup
+        methodBasic()
+    elif im == "custom":
+        #run the custom setup
+        methodCustom()
+    else:
+        # run the express setup
+        print("express")
+        methodExpress()
+
+
+methodChoice(im)
