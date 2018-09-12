50567b2926832ab315e81085864b21034a5f1192
user4802 <24318149+User4802@users.noreply.github.com>
2018-09-12 15:48:23 -0400

    v 0.45

--- projectDeployKit.py
+++ projectDeployKit.py
@@ -1,40 +1,18 @@
-version = "0.4"
+version = "0.45"
 
 from sys import argv
-script, initMethod = argv
+script, deployType = argv
 import os
 
+userType = str(deployType)
+localDir = os.getcwd()
 
 
-im = str(initMethod)
+# EXPRESS DEPLOY BELOW THIS LINE
 
-def methodBasic():
+def methodExpress(type, dir): #CREATE THE STRUCTURE IN THE CURRENT DIRECTORY, FOLDER NAME SPECIFIED IN THE ARGV
 
-    print("Project Deploy Kit V{0}".format(version))
-
-    express_path = input("Main Path ? ")
-
-    os.mkdir(main_path)
-    os.mkdir(main_path + "/plans")
-    os.mkdir(main_path + "/documents")
-    os.mkdir(main_path + "/soumission")
-    os.mkdir(main_path + "/cotations")
-
-def methodCustom():
-
-    print("Project Deploy Kit V{0}".format(version))
-
-    main_path = input("Main Path ? ")
-
-    os.mkdir(main_path)
-    os.mkdir(main_path + "/plans")
-    os.mkdir(main_path + "/documents")
-    os.mkdir(main_path + "/soumission")
-    os.mkdir(main_path + "/cotations")
-
-def methodExpress():
-
-    express_path = os.getcwd() + "/" + im
+    express_path = dir + "/" + type
     os.mkdir(express_path)
     os.mkdir(express_path + "/plans")
     os.mkdir(express_path + "/documents")
@@ -42,17 +20,52 @@ def methodExpress():
     os.mkdir(express_path + "/cotations")
     os.mkdir(express_path + "/dossier")
 
-def methodChoice(im):
-    if im == "basic":
+    print("\nThe following folder : {0}, have been created at this location: {1}\n".format(type, dir))
+
+# BASIC DEPLOY BELOW THIS LINE
+
+def methodBasic(): #CREATE THE STRUCTURE IN A USER SPECIFIED DIRECTORY
+
+    print("Project Deploy Kit V{0}".format(version))
+
+    basic_choice = input("Use the current directory or specify a new one ? ")
+
+    if basic_choice == "current":
+        basic_current = os.getcwd()
+        basic_folder = str(input("folder name ? "))
+        basic_path = basic_current + "/" + basic_folder
+        os.mkdir(basic_path)
+        os.mkdir(basic_path + "/plans")
+        os.mkdir(basic_path + "/documents")
+        os.mkdir(basic_path + "/soumission")
+        os.mkdir(basic_path + "/cotations")
+        os.mkdir(basic_path + "/dossier")
+
+        print("\nThe following folder : {0}, have been created at this location: {1}\n".format(basic_folder, basic_current))
+
+    elif basic_choice == "new":
+        print("new")
+    else:
+        print("not a valid input")
+        methodBasic()
+
+# CUSTOM DEPLOY BELOW THIS LINE
+
+def methodCustom(): #CREATE THE STRUCTURE IN A USER SPECIFIED DIRECTORY + CUSTOM NAME, CUSTOM SUBFOLDER, CUSTOM AMOUNT
+    print("custom")
+
+# CHOICE EVALUATING below this line
+
+def methodChoice(type, dir):
+    if type == "basic":
         #run the basic setup
         methodBasic()
-    elif im == "custom":
+    elif type == "custom":
         #run the custom setup
         methodCustom()
     else:
         # run the express setup
-        print("express")
-        methodExpress()
+        methodExpress(type, dir)
 
 
-methodChoice(im)
+methodChoice(userType, localDir)
